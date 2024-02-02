9514c1a0748a5ad68e9ec168b0d88bad274a139b
Emiel Harmsen <emielch@gmail.com>
2024-02-02 15:49:21 +0800

    lock on audioData

--- Scripts/Audio/OKAudioListener.cs
+++ Scripts/Audio/OKAudioListener.cs
@@ -18,6 +18,8 @@ public class OKAudioListener : MonoBehaviour {
     short[] audioData;
     int aDataIdx = 0;
 
+    System.Object audioDataLock = new System.Object();
+
     public float newSampleRate = 44100;
     public float targetQueueFill = 0.7f;
     public float currentQueueFill = 0.6f;
@@ -54,45 +56,53 @@ public class OKAudioListener : MonoBehaviour {
     }
 
     void ProcessAudio() {
-        currentQueueFill = device.audioQueueLevel;
-        float queueFillDifference = currentQueueFill - targetQueueFill;
-        newSampleRate = OKAudioManager.instance.sampleRate - (queueFillDifference * adjustmentSpeed);
+        if (Monitor.TryEnter(audioDataLock)) {
+            try {
+                currentQueueFill = device.audioQueueLevel;
+                float queueFillDifference = currentQueueFill - targetQueueFill;
+                newSampleRate = OKAudioManager.instance.sampleRate - (queueFillDifference * adjustmentSpeed);
 
-        float prevSample = samples[samplesLen - 1];
-        Array.Fill(samples, 0f);
-        samplesLen = OKAudioManager.GetFL();
+                float prevSample = samples[samplesLen - 1];
+                Array.Fill(samples, 0f);
+                samplesLen = OKAudioManager.GetFL();
 
-        inputIndex %= 1;
-        int startCount = 0;
-        if (inputIndex != 0) {
-            samples[0] = prevSample;
-            startCount = 1;
-            samplesLen++;
-        }
+                inputIndex %= 1;
+                int startCount = 0;
+                if (inputIndex != 0) {
+                    samples[0] = prevSample;
+                    startCount = 1;
+                    samplesLen++;
+                }
 
-        foreach (var aSource in OKAudioManager.instance.audioSources) {
-            if (!aSource.HasSamples()) continue;
-            float dist = Vector3.Distance(aSource.currPos, currPos);
-            if (dist > aSource.maxDistance) continue;
-            float distVol = aSource.isSpatial ? Mathf.Min(1, aSource.rolloffScale / (Mathf.Max(aSource.minDistance, dist) - aSource.minDistance + aSource.rolloffScale)) : 1;
-            float _vol = vol * distVol * OKAudioManager.instance.masterVol;
-            for (int i = startCount; i < samplesLen; i++) {
-                samples[i] += aSource.GetSample(i) * _vol;
+                foreach (var aSource in OKAudioManager.instance.audioSources) {
+                    if (!aSource.HasSamples()) continue;
+                    float dist = Vector3.Distance(aSource.currPos, currPos);
+                    if (dist > aSource.maxDistance) continue;
+                    float distVol = aSource.isSpatial ? Mathf.Min(1, aSource.rolloffScale / (Mathf.Max(aSource.minDistance, dist) - aSource.minDistance + aSource.rolloffScale)) : 1;
+                    float _vol = vol * distVol * OKAudioManager.instance.masterVol;
+                    for (int i = startCount; i < samplesLen; i++) {
+                        samples[i] += aSource.GetSample(i) * _vol;
+                    }
+                }
+
+                float factor = OKAudioManager.instance.sampleRate / newSampleRate;
+                for (; inputIndex < samplesLen - 1; inputIndex += factor) {
+                    int index1 = Mathf.FloorToInt(inputIndex);
+                    int index2 = Mathf.CeilToInt(inputIndex);
+
+                    float fraction = inputIndex - index1;
+
+                    audioData[aDataIdx++] = (short)((1 - fraction) * samples[index1] + fraction * samples[index2]);
+                    if (aDataIdx >= samplesInBlock) {
+                        aDataIdx = 0;
+                        device?.SendAudio(audioData);
+                    }
+                }
+            } finally {
+                Monitor.Exit(audioDataLock);
             }
-        }
-
-        float factor = OKAudioManager.instance.sampleRate / newSampleRate;
-        for (; inputIndex < samplesLen - 1; inputIndex += factor) {
-            int index1 = Mathf.FloorToInt(inputIndex);
-            int index2 = Mathf.CeilToInt(inputIndex);
-
-            float fraction = inputIndex - index1;
-
-            audioData[aDataIdx++] = (short)((1 - fraction) * samples[index1] + fraction * samples[index2]);
-            if (aDataIdx >= samplesInBlock) {
-                aDataIdx = 0;
-                device?.SendAudio(audioData);
-            }
+        } else {
+            Debug.Log("unable to enter audioDataLock");
         }
     }
 }
